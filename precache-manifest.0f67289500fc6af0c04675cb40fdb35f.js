c9c4a711b49056ec59cf6c8e20d673bf23d0cf88
Makinde Opeyemi <mankindofficial1@gmail.com>
2020-09-06 15:02:59 +0100

    Updates

---
+++ precache-manifest.0f67289500fc6af0c04675cb40fdb35f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25cc72de49419a8646414be6c3306de",
+    "url": "/TodoList-App---ReactHoks/index.html"
+  },
+  {
+    "revision": "984747e4e737f1767210",
+    "url": "/TodoList-App---ReactHoks/static/css/main.e09c2d40.chunk.css"
+  },
+  {
+    "revision": "7922b40e5fa6d2399618",
+    "url": "/TodoList-App---ReactHoks/static/js/2.0e93e446.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList-App---ReactHoks/static/js/2.0e93e446.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984747e4e737f1767210",
+    "url": "/TodoList-App---ReactHoks/static/js/main.1a11dec5.chunk.js"
+  },
+  {
+    "revision": "e6b6075e6fc5efbb9088",
+    "url": "/TodoList-App---ReactHoks/static/js/runtime-main.ff917453.js"
+  }
+]);
